30b679400e165cf808c94af48635ff1939882bf3
stone0338 <42095371+stone0338@users.noreply.github.com>
2020-04-19 11:50:30 -0400

    Create dunnett_test.R

---
+++ dunnett_test.R
@@ -0,0 +1,18 @@
+dunnett_test <- function(values, groups) {
+  library(reshape2)
+  library(multcomp)
+  
+  long <- data.frame(groups, values)
+  
+  fit <- aov(values ~ groups, long)
+  Dunnet <- glht(fit, linfct=mcp(groups="Dunnett"))
+  
+  pvalues = summary(Dunnet)$test$pvalues
+  
+  num_comparisons = length(pvalues)
+  output = pvalues[1]
+  for (p_value in pvalues[2:num_comparisons]) {
+    output = paste0(output, "&" , p_value)
+  }
+  output
+}
