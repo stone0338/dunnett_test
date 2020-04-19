dbdcd557ecc7c1778c7075c8044e0e6f852d45db
Lei Wang <stone0338@gmail.com>
2020-04-19 12:03:26 -0400

    v1.0

--- dunnett_test.R
+++ dunnett_test.R
@@ -1,14 +1,10 @@
 dunnett_test <- function(values, groups) {
   library(reshape2)
   library(multcomp)
-  
   long <- data.frame(groups, values)
-  
   fit <- aov(values ~ groups, long)
   Dunnet <- glht(fit, linfct=mcp(groups="Dunnett"))
-  
   pvalues = summary(Dunnet)$test$pvalues
-  
   num_comparisons = length(pvalues)
   output = pvalues[1]
   for (p_value in pvalues[2:num_comparisons]) {
